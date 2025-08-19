c6083ccc3980392c453f449f73e30c080c11b1a8
jackcat13 <christophehenry11@gmail.com>
2025-08-19 16:22:18 +0200

    feat: Implement menu with start game button

--- src/war_of_progress.c
+++ src/war_of_progress.c
@@ -1,45 +1,64 @@
 #include "raylib.h"
 
-#if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
-#endif
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 
-Camera camera = { 0 };
-Vector3 cubePosition = { 0 };
+#include <stdio.h>
+
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
 
 static void UpdateDrawFrame(void);
+static void RenderMenu(void);
+static void RenderMainGame(void);
 
-int main()
-{
-    InitWindow(0, 0, "War of progress");
+enum Scene { MENU, MAIN_GAME };
+enum Scene current_scene = MENU;
 
-    camera.position = (Vector3){ 3.0f, 3.0f, 2.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
+int main() {
+  InitWindow(0, 0, "War of progress");
+  ToggleFullscreen();
+  GuiLoadStyleDefault();
 
 #if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+  emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 #else
-    SetTargetFPS(60);
+  SetTargetFPS(60);
 
-    while (!WindowShouldClose())
-    {
-        UpdateDrawFrame();
-    }
+  while (!WindowShouldClose()) {
+    UpdateDrawFrame();
+  }
 #endif
-    CloseWindow();
-    return 0;
+  CloseWindow();
+  return 0;
 }
 
-static void UpdateDrawFrame(void)
-{
-    UpdateCamera(&camera, CAMERA_ORBITAL);
-    BeginDrawing();
+static void UpdateDrawFrame(void) {
+  BeginDrawing();
 
-        ClearBackground(RAYWHITE);
-        DrawFPS(10, 10);
+  switch (current_scene) {
+  case MENU:
+    RenderMenu(); break;
+  case MAIN_GAME:
+    RenderMainGame(); break;
+  }
 
-    EndDrawing();
+  EndDrawing();
+}
+
+static void RenderMenu(void) {
+  ClearBackground(BLACK);
+  if (GuiButton((Rectangle){24, 24, 120, 30}, "Start game")) {
+    current_scene = MAIN_GAME;
+  }
+}
+
+const int MARGIN = 20;
+
+static void RenderMainGame(void) {
+  float screen_width = GetScreenWidth();
+  float screen_height = GetScreenHeight();
+  ClearBackground(BLACK);
+  DrawFPS(screen_width - MARGIN, MARGIN);
 }
